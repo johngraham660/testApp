e0eacc5153b9dca504b3c74377e512a100f85a75
John Graham <john.graham@virtua-enterprise.co.uk>
2018-09-04 21:43:35 +0100

    Updated code to print container hostname

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 #! /usr/bin/env/python
 
+import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "<h1>FlaskApp - (c) Virtua Enterprise Limited</h1>" \
-  "FlaskApp, running in a Docker Container! :-)"
+
+    hostname = socket.gethostname()
+
+    message = "<h1>FlaskApp - (c) Virtua Enterprise Limited</h1> \
+               FlaskApp, running in Container \
+               <BR> \
+               Hostname: %s" % hostname
+
+    return message
 
 
 if __name__ == '__main__':
